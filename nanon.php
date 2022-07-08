53e3b1e2b540b6bf50fa631db3a84d9095e416a9
NINEVPN <103882719+NINEVPN@users.noreply.github.com>
2022-07-08 21:08:52 +0700

    Update nanon.php

--- nanon.php
+++ nanon.php
@@ -1,6 +1,6 @@
 {
   "Version_update": "1.0",
-  "Link_zip": "https://nanonvpn.net/update/nanon.zip",
+  "Link_zip": "https://github.com/NINEVPN/nanon/raw/main/nanon.zip",
   "Password": "111111",
   "Message": "NANON VPN"
 }
