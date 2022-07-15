03dc616bd9777128a7cf2ffa4aad38d1cf461b2a
NINEVPN <103882719+NINEVPN@users.noreply.github.com>
2022-07-15 18:07:57 +0700

    Update nanon.php

--- nanon.php
+++ nanon.php
@@ -1,6 +1,16 @@
 {
-  "Version_update": "1.0",
+  "Version_update": "1.2",
   "Link_zip": "https://github.com/NINEVPN/nanon/raw/main/nanon.zip",
-  "Password": "111111",
+  "Password": "140690",
   "Message": "NANON VPN"
+15/07/2022
+เล่นทั่วไป ดูหนัง ฟังเพลง
+A-TRUE-NOPRO-CAT-TH-01
+B-DTAC-NOPRO-CAT-TH-01
+C-AIS-PLAY-CAT-TH-01
+สำหรับเล่นเกมส์
+A-TRUE-NOPRO-GAMING-01
+B-DTAC-NOPRO-GAMING-01
+C-AIS-PLAY-GAMING-01
+"
 }
